65cc8be0fb2d7d9230b5c85e7fad9be99f85de1e
trbogi <trencsenyi.bogi@gmail.com>
2022-05-30 13:52:58 +0200

    feat: add ProductCategory class

---
+++ src/main/java/com/codecool/cms/model/ProductCategory.java
@@ -0,0 +1,29 @@
+package com.codecool.cms.model;
+
+import java.util.UUID;
+
+public class ProductCategory {
+    private UUID id;
+    private String name;
+
+    public ProductCategory(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
