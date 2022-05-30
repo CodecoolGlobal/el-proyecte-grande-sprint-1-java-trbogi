c2a5a8994470589b67106e8936c85981914a7041
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-05-30 12:49:51 +0200

    change: add password field to User class

--- src/main/java/com/codecool/cms/model/User.java
+++ src/main/java/com/codecool/cms/model/User.java
@@ -4,23 +4,29 @@ import java.nio.file.Path;
 import java.util.UUID;
 
     public class User {
+
+        // Field(s)
         private final UUID id;
         private String name;
         private String email;
         private int phone;
+        private String password;
         private UserRole role;
         private Path profilePicture;
         private Address address;
 
-        public User(UUID id, String name, String email, int phone, UserRole role, Path profilePicture) {
+        // Constructor(s)
+        public User(UUID id, String name, String email, int phone, String password, UserRole role, Path profilePicture) {
             this.id = id;
             this.name = name;
             this.email = email;
             this.phone = phone;
+            this.password = password;
             this.role = role;
             this.profilePicture = profilePicture;
         }
 
+        // Getter(s), Setter(s)
         public UUID getId() {
             return id;
         }
@@ -29,44 +35,52 @@ import java.util.UUID;
             return name;
         }
 
-        public String getEmail() {
-            return email;
-        }
-
-        public int getPhone() {
-            return phone;
-        }
-
-        public UserRole getRole() {
-            return role;
-        }
-
-        public Path getProfilePicture() {
-            return profilePicture;
-        }
-
-        public Address getAddress() {
-            return address;
-        }
-
         public void setName(String name) {
             this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
         }
 
         public void setEmail(String email) {
             this.email = email;
         }
 
+        public int getPhone() {
+            return phone;
+        }
+
         public void setPhone(int phone) {
             this.phone = phone;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public UserRole getRole() {
+            return role;
         }
 
         public void setRole(UserRole role) {
             this.role = role;
         }
 
+        public Path getProfilePicture() {
+            return profilePicture;
+        }
+
         public void setProfilePicture(Path profilePicture) {
             this.profilePicture = profilePicture;
+        }
+
+        public Address getAddress() {
+            return address;
         }
 
         public void setAddress(Address address) {
