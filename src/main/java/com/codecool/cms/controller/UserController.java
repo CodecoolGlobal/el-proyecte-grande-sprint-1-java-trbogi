79c31940968c5d25fd60451c8eeee59ed4b3b3ce
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-05-31 15:07:35 +0200

    feat: add UserController class

---
+++ src/main/java/com/codecool/cms/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.codecool.cms.controller;
+
+import com.codecool.cms.dto.NewUserDto;
+import com.codecool.cms.dto.UserDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user/")
+public class UserController {
+
+    // Create new user
+    @PostMapping("post/new")
+    public void createUser(@RequestBody NewUserDto newUserData) {
+    }
+
+    // Get user data
+    @GetMapping("get/{userID}")
+    public UserDto getUserByUserId(@PathVariable String userID) {
+        return null;
+    }
+
+    // Modify user data
+    @PatchMapping("patch/{userID}")
+    public void ModifyUserByUserId(@PathVariable String userID, @RequestBody UserDto modifiedUserData) {
+    }
+}
