6adab27aa04db450b9bd764afa70abf7937e2776
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-06-01 15:08:17 +0200

    feat: extend ReservationController class withadditional functions

--- src/main/java/com/codecool/cms/controller/ReservationController.java
+++ src/main/java/com/codecool/cms/controller/ReservationController.java
@@ -1,17 +1,28 @@
 package com.codecool.cms.controller;
 
+import com.codecool.cms.dto.ReservationDto;
 import com.codecool.cms.model.Court;
 import com.codecool.cms.model.Reservation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/reservation/")
 public class ReservationController {
+
+    // Add new reservation
+    @PostMapping("post/new")
+    public void addNewReservation(@RequestBody ReservationDto reservation) {
+    }
+
+    // Get reservations of the given week by court
+    @GetMapping("get/{courtNumber}/{firstDayOfWeek}")
+    public List<Reservation> getReservationsOfWeekByCourt(@PathVariable Integer courtNumber,
+                                                          @PathVariable LocalDateTime firstDayOfWeek) {
+        return null;
+    }
 
     // Get courts
     @GetMapping("get/courts/")
@@ -19,10 +30,15 @@ public class ReservationController {
         return null;
     }
 
-    // Get reservations by court
-    @GetMapping("get/{courtNumber}")
-    public List<Reservation> getReservationsByCourt(@PathVariable Integer courtNumber) {
-        return null;
+    // Remove coach from reservation
+    @PatchMapping("patch/coach/{reservationId}")
+    public void removeCoachFromReservation(@PathVariable String reservationId) {
+    }
+
+    // Remove participant from practice
+    @PatchMapping("patch/participant/{reservationId}/{userId}")
+    public void removeParticipantFromReservation(@PathVariable String reservationId, @PathVariable String userId) {
     }
 
 }
+
