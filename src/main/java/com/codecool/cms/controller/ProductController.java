d04792dc5fb608d01e0ece51255f8797ee176690
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-06-01 10:43:07 +0200

    feat: add ProductController class

---
+++ src/main/java/com/codecool/cms/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.codecool.cms.controller;
+
+import com.codecool.cms.model.Product;
+import com.codecool.cms.model.ProductCategory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product/")
+public class ProductController {
+
+    // Get product categories
+    @GetMapping("get/categories/")
+    public ProductCategory getCartContentByUserId() {
+        return null;
+    }
+
+    // Get products of a category
+    @GetMapping("get/{categoryId}")
+    public List<Product> getProductsOfCategory(@PathVariable String categoryId) {
+        return null;
+    }
+
+}
