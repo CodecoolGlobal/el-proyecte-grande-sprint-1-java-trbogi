9ca09477c9a3e81b4b90fb86d2a0127a70bc4434
trbogi <trencsenyi.bogi@gmail.com>
2022-06-02 13:03:12 +0200

    Add reservation property to Days

--- src/components/Days.js
+++ src/components/Days.js
@@ -1,9 +1,9 @@
 import Day from "./Day";
 
-function Days({days}) {
+function Days({days, reservations}) {
     return (
         <div className="days">
-            {days.map((day) =>{return <Day day={day}></Day>})}
+            {days.map((day) =>{return <Day day={day} reservations={reservations}></Day>})}
         </div>
     );
 }export default Days
