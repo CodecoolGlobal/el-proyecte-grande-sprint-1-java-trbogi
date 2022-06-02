e3ef69013b42019bbb4544c98706ce6bc81e6dc3
trbogi <trencsenyi.bogi@gmail.com>
2022-06-02 13:02:23 +0200

    Add fake reservation data to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import {useState} from "react";
-import moment from 'moment';
 import TimeTable from './components/TimeTable.js'
 import './style/main.css'
 
 function App() {
+    const reservations = [{'date': '2022-06-03', 'start': '10:00'},
+        {'date': '2022-06-03', 'start': '12:00'},
+        {'date': '2022-06-04', 'start': '10:00'},
+        {'date': '2022-06-07', 'start': '10:00'},
+        {'date': '2022-06-09', 'start': '08:00'},
+        {'date': '2022-06-23', 'start': '08:00'},
+        {'date': '2022-06-15', 'start': '08:00'}]
   return (
     <div className="container" >
         <div>
-            <TimeTable></TimeTable>
+            <TimeTable reservations={reservations}></TimeTable>
         </div>
     </div>
   );
