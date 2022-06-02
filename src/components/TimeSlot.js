019d6dce15cf7236f5ed1cb3c8e95c31056a855d
trbogi <trencsenyi.bogi@gmail.com>
2022-06-02 14:29:56 +0200

    Add reservation to "cart":unreserved TimeSlot turns blue on click

--- src/components/TimeSlot.js
+++ src/components/TimeSlot.js
@@ -1,16 +1,22 @@
 function TimeSlot({day, start, reservations}) {
     const isReserved = () => {
         for (const reservation of reservations) {
-            if (reservation.date == day && reservation.start == start){
+            if (reservation.date === day && reservation.start === start){
                 return true;
             }
         }
         return false;
     }
 
+    const addReservationToCart = (e) => {
+        //TODO: add reservation to cart in backend
+        console.log("reserve")
+        e.target.style.backgroundColor = "#c9d6e8"
+    }
+
     if (isReserved()){
         return(<p style={{backgroundColor: "#fcb7b4"}}>{start}</p>)
     }else{
-        return(<p style={{backgroundColor: "aliceblue"}}>{start}</p>)
+        return(<p style={{backgroundColor: "aliceblue"}} onClick={addReservationToCart}>{start}</p>)
     }
 }export default TimeSlot
