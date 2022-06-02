f518973f7f514f4da2e8fbc1733f4deced0a3291
trbogi <trencsenyi.bogi@gmail.com>
2022-06-02 16:14:03 +0200

    Add key to Day components

--- src/components/Week.js
+++ src/components/Week.js
@@ -3,7 +3,7 @@ import Day from "./Day";
 function Week({days, reservations}) {
     return (
         <div className="days">
-            {days.map((day) =>{return <Day day={day} reservations={reservations}></Day>})}
+            {days.map((day) =>{return <Day key={day} day={day} reservations={reservations}></Day>})}
         </div>
     );
 }export default Week
