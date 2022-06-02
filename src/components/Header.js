92d31acad078f1f2d7025fa429f2fe0fdb47e098
trbogi <trencsenyi.bogi@gmail.com>
2022-06-02 15:35:55 +0200

    Display selected court

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import Moment from "react-moment";
-import Week from "./Week";
+import {useState} from "react";
 
 function Header({startOfWeek}) {
+    const courts = ['Court 1', 'Court 2', 'Court 3', 'Court 4']
+    const [selectedCourt, setSelectedCourt] = useState(null);
+
+    const selectCourt = (court) => {
+        setSelectedCourt(court);
+    }
+
     return(
         <div>
             <div className="courts">
-                <span>Court 1</span>
-                <span>Court 2</span>
-                <span>Court 3</span>
-                <span>Court 4</span>
+                {courts.map((court) => {
+                    return <span key={court} className={selectedCourt === court ? "selectedCourt" : null} onClick={() => {selectCourt(court)}}>{court}</span>
+                })}
             </div>
             <div style={{fontSize: '1.8rem', fontWeight: '900', textAlign: 'center'}}><Moment format={"YYYY. MMMM"}>{startOfWeek}</Moment></div>
         </div>
