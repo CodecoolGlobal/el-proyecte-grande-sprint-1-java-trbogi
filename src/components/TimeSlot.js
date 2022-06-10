aa85b347e7d39492bc855f7a146cd8665aa981f4
trbogi <trencsenyi.bogi@gmail.com>
2022-06-10 12:34:07 +0200

    Add inCart state to TimeSlot

--- src/components/TimeSlot.js
+++ src/components/TimeSlot.js
@@ -1,4 +1,8 @@
+import {BsCartPlus, BsFillCartCheckFill} from "react-icons/bs";
+import {useState} from "react";
+
 function TimeSlot({day, start, reservations}) {
+    const [inCart, setInCart] = useState(false);
     const isReserved = () => {
         for (const reservation of reservations) {
             if (reservation.date === day && reservation.start === start){
@@ -11,12 +15,16 @@ function TimeSlot({day, start, reservations}) {
     const addReservationToCart = (e) => {
         //TODO: add reservation to cart in backend
         console.log("reserve")
-        e.target.style.backgroundColor = "#c9d6e8"
+        setInCart(true);
     }
 
     if (isReserved()){
         return(<p style={{backgroundColor: "#fcb7b4"}}>{start}</p>)
+    }else if(inCart){
+        return (<p style={{backgroundColor: "#c9d6e8"}}>{start} <br/>
+            <BsFillCartCheckFill></BsFillCartCheckFill></p>)
     }else{
-        return(<p style={{backgroundColor: "aliceblue"}} onClick={addReservationToCart}>{start}</p>)
+        return(<p style={{backgroundColor: "aliceblue"}} onClick={addReservationToCart}>{start} <br/>
+            <BsCartPlus></BsCartPlus></p>)
     }
 }export default TimeSlot
