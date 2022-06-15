c5d4ba348f39950d457f1554a339a904f48c9838
trbogi <trencsenyi.bogi@gmail.com>
2022-06-15 17:31:47 +0200

    Example of reservations from App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,9 @@ import TimeTable from './components/TimeTable.js'
 import './style/main.css'
 
 function App() {
-    const reservations = [{'date': '2022-06-03', 'start': '10:00'},
-        {'date': '2022-06-03', 'start': '12:00'},
-        {'date': '2022-06-04', 'start': '10:00'},
-        {'date': '2022-06-07', 'start': '10:00'},
-        {'date': '2022-06-09', 'start': '08:00'},
-        {'date': '2022-06-23', 'start': '08:00'},
-        {'date': '2022-06-15', 'start': '08:00'}]
   return (
     <div className="container" >
-        <TimeTable reservations={reservations}></TimeTable>
+        <TimeTable></TimeTable>
     </div>
   );
 }
