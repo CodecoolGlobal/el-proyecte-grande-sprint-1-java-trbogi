e5fe3c63d943f88cd10a295e3deb0321c4d9004f
trbogi <trencsenyi.bogi@gmail.com>
2022-06-17 14:01:35 +0200

    feat: create CartRepository

---
+++ src/main/java/com/codecool/cms/data/CartRepository.java
@@ -0,0 +1,9 @@
+package com.codecool.cms.data;
+
+import com.codecool.cms.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CartRepository extends JpaRepository<Cart, UUID> {
+}
