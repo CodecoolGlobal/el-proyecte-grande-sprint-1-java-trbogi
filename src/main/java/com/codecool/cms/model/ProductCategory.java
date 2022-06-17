72819b0058539b3f199617a02c8c84f41e07b0f5
trbogi <trencsenyi.bogi@gmail.com>
2022-06-17 14:03:19 +0200

    change: make ProductCategory as an Entity

--- src/main/java/com/codecool/cms/model/ProductCategory.java
+++ src/main/java/com/codecool/cms/model/ProductCategory.java
@@ -1,10 +1,16 @@
 package com.codecool.cms.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.UUID;
 
+@Entity
 public class ProductCategory {
 
     // Field(s)
+    @Id
+    @GeneratedValue
     private UUID id;
     private String name;
 
@@ -12,6 +18,10 @@ public class ProductCategory {
     public ProductCategory(UUID id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public ProductCategory() {
+
     }
 
     // Getter(s), Setter(s)
