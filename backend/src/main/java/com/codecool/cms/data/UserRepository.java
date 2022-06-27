4a126a46c61ed6af5c9a3a5dff109529a8e68e4c
trbogi <trencsenyi.bogi@gmail.com>
2022-06-27 16:00:27 +0200

    feat: create UserRepository

---
+++ backend/src/main/java/com/codecool/cms/data/UserRepository.java
@@ -0,0 +1,9 @@
+package com.codecool.cms.data;
+
+import com.codecool.cms.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
