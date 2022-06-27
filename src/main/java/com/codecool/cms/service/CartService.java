a3ee5ad7d7570c91ab64f2fcc803b9c4c1de73a2
trbogi <trencsenyi.bogi@gmail.com>
2022-06-27 10:43:06 +0200

    feat: add addReservation method to CartService

--- src/main/java/com/codecool/cms/service/CartService.java
+++ src/main/java/com/codecool/cms/service/CartService.java
@@ -1,10 +1,13 @@
 package com.codecool.cms.service;
 
 import com.codecool.cms.data.CartRepository;
+import com.codecool.cms.data.ReservationRepository;
 import com.codecool.cms.model.Cart;
+import com.codecool.cms.model.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -12,8 +15,19 @@ public class CartService {
 
     @Autowired
     CartRepository cartRepository;
+    @Autowired
+    ReservationRepository reservationRepository;
 
     public void createCartForUser(UUID userId){
 
+    }
+
+    public void addReservationToCart(UUID reservationId, UUID cartId){
+        Cart cart = cartRepository.getById(cartId);
+        Reservation reservation = reservationRepository.getById(reservationId);
+        List<Reservation> reservations = cart.getReservations();
+        reservations.add(reservation);
+        cart.setReservations(reservations);
+        cartRepository.save(cart);
     }
 }
