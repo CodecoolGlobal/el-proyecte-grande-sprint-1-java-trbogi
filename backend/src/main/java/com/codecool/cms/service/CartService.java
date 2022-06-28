cd8281c47c6881eee2391350a0ff70fcd23d92c4
trbogi <trencsenyi.bogi@gmail.com>
2022-06-28 12:22:14 +0200

    feat: add addReservationToCart method CartService

--- backend/src/main/java/com/codecool/cms/service/CartService.java
+++ backend/src/main/java/com/codecool/cms/service/CartService.java
@@ -1,12 +1,14 @@
 package com.codecool.cms.service;
 
 import com.codecool.cms.data.CartRepository;
-import com.codecool.cms.data.ReservationRepository;
+import com.codecool.cms.data.UserRepository;
 import com.codecool.cms.model.Cart;
 import com.codecool.cms.model.Reservation;
+import com.codecool.cms.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,16 +17,20 @@ public class CartService {
 
     @Autowired
     CartRepository cartRepository;
+
     @Autowired
-    ReservationRepository reservationRepository;
+    UserRepository userRepository;
 
-    public void createCartForUser(UUID userId){
+    @Autowired
+    ReservationService reservationService;
 
+    public void createCartForUser(User user){
+        cartRepository.save(new Cart(user));
     }
 
-    public void addReservationToCart(UUID reservationId, UUID cartId){
-        Cart cart = cartRepository.getById(cartId);
-        Reservation reservation = reservationRepository.getById(reservationId);
+    public void addReservationToCart(UUID userId, LocalDateTime startTime, int courtNumber){
+        Reservation reservation = reservationService.createReservation(userId, startTime, courtNumber);
+        Cart cart = cartRepository.getCartByUser(userRepository.getById(userId));
         List<Reservation> reservations = cart.getReservations();
         reservations.add(reservation);
         cart.setReservations(reservations);
