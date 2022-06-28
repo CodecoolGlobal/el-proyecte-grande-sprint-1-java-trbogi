1db3ef5e19c67d68678f9d16f0c26cf857f6554d
trbogi <trencsenyi.bogi@gmail.com>
2022-06-28 12:19:02 +0200

    feat: add getCartByUser method to CartRepository

--- backend/src/main/java/com/codecool/cms/data/CartRepository.java
+++ backend/src/main/java/com/codecool/cms/data/CartRepository.java
@@ -1,9 +1,11 @@
 package com.codecool.cms.data;
 
 import com.codecool.cms.model.Cart;
+import com.codecool.cms.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, UUID> {
+    Cart getCartByUser(User user);
 }
