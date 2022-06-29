b6abde607954615e5ab8a4c06e4e53fe2611dab4
trbogi <trencsenyi.bogi@gmail.com>
2022-06-29 11:03:20 +0200

    feat: add cross origin annotation to Cartcontroller

--- backend/src/main/java/com/codecool/cms/controller/CartController.java
+++ backend/src/main/java/com/codecool/cms/controller/CartController.java
@@ -11,6 +11,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/cart/")
 public class CartController {
