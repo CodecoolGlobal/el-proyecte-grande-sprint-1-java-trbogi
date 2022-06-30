0fa1fa08e4b7ca2290723cefad313d6422838b7e
trbogi <trencsenyi.bogi@gmail.com>
2022-06-30 15:31:49 +0200

    feat: login from frontend

--- frontend/src/components/user_management/Login.js
+++ frontend/src/components/user_management/Login.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import {SyntheticEvent, useState} from "react";
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submit = (e: SyntheticEvent) => {
+        e.preventDefault();
+        fetch("http://localhost:8080/api/user/login", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: new URLSearchParams({
+                "username": username,
+                "password": password
+            })
+        }).then(r => r.json()).then(data => {
+            console.log('Success:', data)})
+        }
+
+
     return (
-        <div>Login</div>
+        <div>
+            <form onSubmit={submit}>
+                    <h1>Please sign in</h1>
+                    <div>
+                        <label htmlFor="floatingInput">Username</label>
+                        <input type="text" id="floatingInput" placeholder="Username" onChange={event => setUsername(event.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor="floatingPassword">Password</label>
+                        <input type="password"  id="floatingPassword" placeholder="Password" onChange={event => setPassword(event.target.value)}/>
+                    </div>
+                    <button type="submit">Sign in</button>
+            </form>
+        </div>
     )
 }
 
