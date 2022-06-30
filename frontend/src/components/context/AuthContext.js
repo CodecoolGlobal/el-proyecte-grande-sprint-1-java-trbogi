9727cf8bb0cf6cacac2d5a5dc65da6ada58dc4a6
trbogi <trencsenyi.bogi@gmail.com>
2022-07-01 01:01:41 +0200

    feat: add logout feature

--- frontend/src/components/context/AuthContext.js
+++ frontend/src/components/context/AuthContext.js
@@ -1,16 +1,31 @@
 import {createContext, useState, useEffect} from 'react'
+import jwtDecode from "jwt-decode";
 
 const AuthContext = createContext()
 
 export default AuthContext
 
 export const AuthProvider = ({children}) => {
-    const [authTokens, setAuthTokens] = useState(null);
-    const [user, setUser] = useState(null);
-    const [userId, setUserId] = useState(null);
+    const [user, setUser] = useState(() => localStorage.getItem("authTokens") ? jwtDecode(JSON.parse(localStorage.getItem("authTokens"))['access_token']) : null
+    );
+    const [authTokens, setAuthTokens] = useState(() => localStorage.getItem("authTokens") ? JSON.parse(localStorage.getItem("authTokens")) : null);
+
+    const logout = () => {
+        setAuthTokens(null);
+        setUser(null);
+        localStorage.removeItem("authTokens");
+    }
+
+    const contextData = {
+        authTokens: authTokens,
+        user: user,
+        setAuthTokens: setAuthTokens,
+        setUser: setUser,
+        logout: logout
+    }
 
     return(
-        <AuthContext.Provider value={[authTokens, setAuthTokens, user, setUser, userId, setUserId]}>
+        <AuthContext.Provider value={contextData}>
             {children}
         </AuthContext.Provider>
     )
