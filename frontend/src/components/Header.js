45e905a9e248510dee3b5a6e06583509da49c63b
trbogi <trencsenyi.bogi@gmail.com>
2022-07-01 01:02:56 +0200

    feat: add conditional header based on authentication

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 import {useContext} from "react";
 import AuthContext from "./context/AuthContext";
 
-
 const Header = () => {
-    const [user] = useContext(AuthContext);
+    const {authTokens, setAuthTokens, user, setUser, userId, setUserId, logout} = useContext(AuthContext);
     return (
         <div>
             <button><Link to="/shop">Shop</Link></button>
             <button><Link to="/reservation">Reservation</Link></button>
-            <button><Link to="/register">Register</Link></button>
-            {user ? (<span>Welcome {user}!</span>):
-                (<button><Link to="/login">Login</Link></button>)}
+            {user ? (<div>
+                        <span>Welcome {user['sub']}!</span>
+                        <button onClick={() => logout()}>Logout</button>
+                    </div>):
+                (<>
+                    <button><Link to="/register">Register</Link></button><button><Link to="/login">Login</Link></button>
+                </>)}
         </div>
     )
 }
