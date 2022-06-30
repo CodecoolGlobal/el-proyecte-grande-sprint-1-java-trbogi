087cf7c7d89946f5cb02744c80ca014cf3e17dcf
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-06-30 13:22:23 +0200

    feat: add header buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './style/main.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import Home from './components/Home';
 import TimeTable from './components/reservation/TimeTable.js';
 import Shop from './components/shop/Shop.js';
 import Register from './components/user_management/Register';
@@ -13,6 +14,7 @@ function App() {
         <BrowserRouter>
             <Layout>
                 <Routes>
+                    <Route path="/" element={<Home />} />
                     <Route path="/shop" element={<Shop />} />
                     <Route path="/reservation" element={<TimeTable />} />
                     <Route path="/register" element={<Register />} />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,10 +5,12 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <div>
+            <button><Link to="/">Home</Link></button>
             <button><Link to="/shop">Shop</Link></button>
             <button><Link to="/reservation">Reservation</Link></button>
             <button><Link to="/register">Register</Link></button>
             <button><Link to="/login">Login</Link></button>
+            <button id="logout-button">Logout</button>
         </div>
     )
 }
