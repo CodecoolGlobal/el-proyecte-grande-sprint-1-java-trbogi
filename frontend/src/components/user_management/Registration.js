8e061c5d26af34a947814b1fed48b5cbce618542
trbogi <trencsenyi.bogi@gmail.com>
2022-07-25 15:12:21 +0200

    style: registration form

--- frontend/src/components/user_management/Registration.js
+++ frontend/src/components/user_management/Registration.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import '../../style/modal.css';
+import '../../style/registrationmodal.css';
 import {useState} from "react";
 import {AiFillCloseCircle} from "react-icons/ai";
+import {FaLock, FaUserAlt, FaQuestionCircle, FaEnvelope} from "react-icons/fa";
+import UserRoleDropdown from "./UserRoleDropdown";
 
 const Register = (props) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [role, setRole] = useState("MEMBER");
+    const [email, setEmail] = useState("");
+    const [selectedRole, setSelectedRole] = useState("MEMBER");
+
 
     const submitRegistration = async (e) => {
         e.preventDefault();
@@ -16,7 +20,8 @@ const Register = (props) => {
             body: JSON.stringify({
                 "username": username,
                 "password": password,
-                "role": role
+                "email": email,
+                "role": selectedRole
             })
         }).then(r => {
             console.log("status" + r.status);
@@ -28,22 +33,31 @@ const Register = (props) => {
     return (
         <div id="modal-container">
             <div id="modal-fade">
-                <div id="modal-dialog">
+                <div id="registration-modal-dialog">
                     <div id="modal-header">
-                        <div id="modal-title">Registration</div>
                         <div id="modal-close" onClick={props.closeDialog}><AiFillCloseCircle size={20}/></div>
+                        <div id="modal-title">Sign up</div>
                     </div>
                     <div id="modal-content">
-                        <div><div>Username:</div><input type="text" placeholder="Username" onChange={event => setUsername(event.target.value)} /></div>
-                        <label>Who are you?</label>
-                        <select onChange={event => setRole(event.target.value)}>
-                            <option value="MEMBER">Member</option>
-                            <option value="COACH">Coach</option>
-                        </select>
-                        <div><div>Password:</div><input type="password" placeholder="Password" onChange={event => setPassword(event.target.value)}/></div>
-                    </div>
-                    <div id="modal-footer">
-                        <button id="modal-button" onClick={submitRegistration}>Register</button>
+                        <form>
+                            <div className="input-container">
+                                <input type="text" required=" " onChange={event => setUsername(event.target.value)} />
+                                <label><FaUserAlt/> Username</label>
+                            </div>
+                            <div className="input-container">
+                                <input type="email" required=" " onChange={event => setEmail(event.target.value)} />
+                                <label><FaEnvelope/> E-mail</label>
+                            </div>
+                            <div className="input-container">
+                                <label><FaQuestionCircle/> Who are you?</label> <br/>
+                                <UserRoleDropdown selected={selectedRole} setSelected={setSelectedRole}/>
+                            </div>
+                            <div className="input-container">
+                                <input type="password" required=" " onChange={event => setPassword(event.target.value)}/>
+                                <label><FaLock/> Password</label>
+                            </div>
+                        </form>
+                        <button id="modal-button" onClick={submitRegistration}>Sign up</button>
                     </div>
                 </div>
             </div>
