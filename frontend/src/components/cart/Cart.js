6768c54fa4e3ef37be1c951d4e70c26617aa1d17
trbogi <trencsenyi.bogi@gmail.com>
2022-07-26 11:34:09 +0200

    feat: fetch reservations of user's cart

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -1,6 +1,25 @@
+import {useEffect, useState} from "react";
+import {useContext} from "react";
+import AuthContext from "../context/AuthContext";
+
 const Cart = () => {
+    const [reservations, setReservations] = useState([]);
+    const {user} = useContext(AuthContext);
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/cart/get-cart-reservations/${user['userId']}`, {
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                setReservations(data);
+            })
+    }, []);
+
     return (
-        <div>Cart</div>
+        <div>
+            {0 < reservations.length  && <span>ok</span>}
+        </div>
     )
 }
 
