7117cd88ac0d409dff906f774d33a1b7ec581675
trbogi <trencsenyi.bogi@gmail.com>
2022-07-26 09:27:55 +0200

    feat: add getReservationsFromCart method to CartRepository

--- backend/src/main/java/com/codecool/cms/data/CartRepository.java
+++ backend/src/main/java/com/codecool/cms/data/CartRepository.java
@@ -1,11 +1,17 @@
 package com.codecool.cms.data;
 
 import com.codecool.cms.model.Cart;
+import com.codecool.cms.model.Reservation;
 import com.codecool.cms.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, UUID> {
     Cart getCartByUser(User user);
+    @Query(value = "SELECT  cast(reservations_id as varchar) FROM cart_reservations\n " +
+            "WHERE cart_id = ?1", nativeQuery = true)
+    List<Object> getReservationsFromCart(UUID cartId);
 }
