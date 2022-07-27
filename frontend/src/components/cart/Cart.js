0325dccff3cfeaeed560aaf3ca918610345b19ed
trbogi <trencsenyi.bogi@gmail.com>
2022-07-27 11:04:14 +0200

    style: cart

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -1,11 +1,14 @@
-import {useEffect, useState} from "react";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import AuthContext from "../context/AuthContext";
 import ReservationCard from "./ReservationCard";
 
 const Cart = () => {
     const [reservations, setReservations] = useState([]);
     const {user} = useContext(AuthContext);
+    const totalPrice = () => {
+        const prices = reservations.map(res => res['price'])
+        return prices.reduce((a, b) => parseFloat(a) + parseFloat(b), 0);
+    }
 
     useEffect(() => {
         fetch(`http://localhost:8080/api/cart/get-cart-reservations/${user['userId']}`, {
@@ -18,13 +21,28 @@ const Cart = () => {
     }, []);
 
     return (
-        <div className="cards-container">
-            <div className="cards">
-                {0 < reservations.length  &&
-                    reservations.map(reservation => {
-                        return <ReservationCard reservation={reservation}/>})}
+        <>
+            <div className="cards-container">
+                <div className="total">
+                    <h2>YOUR CART</h2>
+                    <p>You have {reservations.length} reservation(s) in your cart.</p>
+                    {reservations.length !== 0 &&
+                        <>
+                            <p>Total Price: <label className="total-price">{totalPrice()} HUF</label></p>
+                            <div>
+                                <span className="delete-all-btn">Delete All</span>
+                                <span className="go-to-checkout-btn">Go To Checkout</span>
+                            </div>
+                        </>
+                    }
+                </div>
+                <div className="cards">
+                    {0 < reservations.length  &&
+                        reservations.map(reservation => {
+                            return <ReservationCard key={reservation['id']} reservation={reservation}/>})}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
