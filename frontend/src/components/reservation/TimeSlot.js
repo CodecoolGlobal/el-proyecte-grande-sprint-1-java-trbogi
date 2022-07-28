6ca03033c8555a1ff55af0af126591195930788d
trbogi <trencsenyi.bogi@gmail.com>
2022-07-28 22:56:59 +0200

    feat: remove reservation from cart

--- frontend/src/components/reservation/TimeSlot.js
+++ frontend/src/components/reservation/TimeSlot.js
@@ -1,5 +1,5 @@
 import {BsBagPlusFill, BsBagDash} from "react-icons/bs";
-import {useContext, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import moment from "moment";
 import {CourtContext} from "./TimeTable";
 import AuthContext from "../context/AuthContext";
@@ -10,6 +10,8 @@ function TimeSlot(props) {
     const slotsStartTime = props.day+ " "+  props.start;
     const [inCart, setInCart] = useState(false);
     const isPast = moment(slotsStartTime).isBefore(moment());
+    const [currentReservation, setCurrentReservation] = useState(null);
+
     const isReserved = () => {
         for (const reservation of props.reservations) {
             const timeData = reservation.startTime.split(" ");
@@ -50,19 +52,47 @@ function TimeSlot(props) {
         })
             .then(response =>{
                 if (response.ok){
-                    console.log('Success')
-                }else if (response.status !== 403){
-                    alert("You have to be a member to book a court!")
-                }})
+                    console.log('Success: put in cart')
+                    response.json()
+                        .then(r => setCurrentReservation(r));
+                }
+            })
             .catch((error) => {
                 console.error('Error:', error);
             });
     }
 
     const removeReservationFromCart = () => {
-        //TODO: remove reservation from cart in backend
-        setInCart(false);
+            fetch(`http://localhost:8080/api/cart/delete-reservation/${currentReservation}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    //'Authentication': 'Bearer ' + authTokens['access_token']
+                },
+            })
+                .then(response =>{
+                    if (response.ok){
+                        console.log('Delete was successful');
+                        setInCart(false);
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
     }
+
+    useEffect(() =>{
+        for (const reservation of props.reservationsInCart) {
+            const timeData = reservation.startTime.split(" ");
+            const reservationDate = timeData[0];
+            const reservationStart = timeData[1];
+            if (reservationDate === props.day && reservationStart === props.start){
+                setCurrentReservation(reservation.id);
+            }
+        }
+        }
+    )
 
     if (isReserved()){
         return(<p style={{backgroundColor: "#fcb7b4"}}>{props.start}</p>)
