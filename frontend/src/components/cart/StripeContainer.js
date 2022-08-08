cbda7770373a4e1ab4e2e6ee1879f04b8a45dfbb
trbogi <trencsenyi.bogi@gmail.com>
2022-08-08 14:21:18 +0200

    feat: create StripeContainer

---
+++ frontend/src/components/cart/StripeContainer.js
@@ -0,0 +1,16 @@
+import {Elements} from '@stripe/react-stripe-js'
+import {loadStripe} from '@stripe/stripe-js'
+
+const StripeContainer = () => {
+    const PUBLIC_KEY = "pk_test_51LUUebJLOuDYsmQO10IoM7xAuJZUiZje38oOtDvzwXkOvMwjgUkVt9oPW78mJgsre4vfspXF7ZGrAlvZt7HuXxD100CZs2cXUe";
+
+    const stripeTestPromise = loadStripe(PUBLIC_KEY);
+
+    return (
+        <Elements stripe={stripeTestPromise}>
+            <PaymentForm/>
+        </Elements>
+    )
+}
+
+export default StripeContainer
