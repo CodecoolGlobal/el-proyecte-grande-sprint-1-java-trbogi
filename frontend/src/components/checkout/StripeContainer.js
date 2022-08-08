6b3252db79918032d85707890cf98dcb3b1e6b19
trbogi <trencsenyi.bogi@gmail.com>
2022-08-08 20:50:13 +0200

    change: create payment intent with reservations in cart

--- frontend/src/components/checkout/StripeContainer.js
+++ frontend/src/components/checkout/StripeContainer.js
@@ -1,13 +1,15 @@
 import {Elements} from '@stripe/react-stripe-js'
 import {loadStripe} from '@stripe/stripe-js'
 import PaymentForm from "./PaymentForm";
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
+import ReservationsContext from "../context/ReservationsContext";
 
 const PUBLIC_KEY = "pk_test_51LUUebJLOuDYsmQO10IoM7xAuJZUiZje38oOtDvzwXkOvMwjgUkVt9oPW78mJgsre4vfspXF7ZGrAlvZt7HuXxD100CZs2cXUe";
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
 const StripeContainer = () => {
+    const {reservationsInCart} = useContext(ReservationsContext)
     const [clientSecret, setClientSecret] = useState("");
 
     useEffect(() => {
@@ -15,7 +17,7 @@ const StripeContainer = () => {
         fetch("http://localhost:8080/api/payment/create-payment-intent", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+            body: JSON.stringify({ items: reservationsInCart }),
         })
             .then((res) => res.json())
             .then((data) => setClientSecret(data.clientSecret));
