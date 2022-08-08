75411a55931c973f2e616292ea0560ef659b5fe5
trbogi <trencsenyi.bogi@gmail.com>
2022-08-08 19:27:24 +0200

    change: StripeContainer

--- frontend/src/components/cart/StripeContainer.js
+++ frontend/src/components/cart/StripeContainer.js
@@ -1,16 +1,41 @@
 import {Elements} from '@stripe/react-stripe-js'
 import {loadStripe} from '@stripe/stripe-js'
 import PaymentForm from "./PaymentForm";
+import {useEffect, useState} from "react";
+
+const PUBLIC_KEY = "pk_test_51LUUebJLOuDYsmQO10IoM7xAuJZUiZje38oOtDvzwXkOvMwjgUkVt9oPW78mJgsre4vfspXF7ZGrAlvZt7HuXxD100CZs2cXUe";
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
 const StripeContainer = () => {
-    const PUBLIC_KEY = "pk_test_51LUUebJLOuDYsmQO10IoM7xAuJZUiZje38oOtDvzwXkOvMwjgUkVt9oPW78mJgsre4vfspXF7ZGrAlvZt7HuXxD100CZs2cXUe";
+    const [clientSecret, setClientSecret] = useState("");
 
-    const stripeTestPromise = loadStripe(PUBLIC_KEY);
+    useEffect(() => {
+        // Create PaymentIntent as soon as the page loads
+        fetch("http://localhost:8080/api/payment/create-payment-intent", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret));
+    }, []);
+
+    const appearance = {
+        theme: 'stripe',
+    };
+    const options = {
+        clientSecret,
+        appearance,
+    };
 
     return (
-        <Elements stripe={stripeTestPromise}>
-            <PaymentForm/>
-        </Elements>
+        <div>
+            {clientSecret && (
+                <Elements stripe={stripeTestPromise} options={options}>
+                    <PaymentForm/>
+                </Elements>)}
+        </div>
     )
 }
 
