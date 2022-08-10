55eef4e2d388b4e733b85eb7c6c66e5f4531b15c
trbogi <trencsenyi.bogi@gmail.com>
2022-08-10 17:20:27 +0200

    change: design home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import background from '../images/homebg.jpg'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="home-container">
+            <div className="home-container-content">
+                <div className="upper">
+                    <div className="popout">
+                        <span>B</span>
+                        <span>E</span>
+                        <span>C</span>
+                        <span>O</span>
+                        <span>M</span>
+                        <span>E</span><br/>
+                        <span>O</span>
+                        <span>U</span>
+                        <span>R</span><br/>
+                        <span>M</span>
+                        <span>E</span>
+                        <span>M</span>
+                        <span>B</span>
+                        <span>E</span>
+                        <span>R</span>
+                        <span>!</span>
+                    </div>
+                </div>
+
+                <p>
+                    Our sport club has 4 professional beach volleyball courts right next to Lake Balaton. <br/>
+                    Play volleyball with your friends. <br/>
+                    Book your court now!
+                </p>
+            </div>
+        </div>
     )
 }
 
