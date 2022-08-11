fddd705f37b5804fa8f3066381bcc9da57712647
trbogi <trencsenyi.bogi@gmail.com>
2022-08-11 10:25:36 +0200

    change: implement calculateOrderAmount in PaymentController

--- backend/src/main/java/com/codecool/cms/controller/PaymentController.java
+++ backend/src/main/java/com/codecool/cms/controller/PaymentController.java
@@ -1,25 +1,31 @@
 package com.codecool.cms.controller;
 
 import com.codecool.cms.dto.CreatePaymentResponse;
+import com.codecool.cms.model.Reservation;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
 import com.codecool.cms.dto.CreatePayment;
 import org.springframework.web.bind.annotation.*;
+import java.math.BigDecimal;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/payment/")
 public class PaymentController {
 
+    static BigDecimal calculateOrderAmount(Object[] items) {
+        return Reservation.DEFAULT_PRICE.multiply(BigDecimal.valueOf(items.length));
+    }
+
     @PostMapping("/create-payment-intent")
     public CreatePaymentResponse getPaymentId(@RequestBody CreatePayment createPayment) throws StripeException {
         Stripe.apiKey = "sk_test_51LUUebJLOuDYsmQOMljuw2KrTWU3CHwuD034dNBD7rTm7OQiWJsARJZ8RqRxsCDyuUX5D8iHlrDrYbsWk9yFOwxI00GkB2MiuD";
         PaymentIntentCreateParams params =
                 PaymentIntentCreateParams.builder()
-                        .setAmount(15 * 100L) //TODO:remove hard coded price (with CreatePayment)
-                        .setCurrency("eur")
+                        .setAmount(new Long(String.valueOf(calculateOrderAmount(createPayment.getItems()))) * 100L)
+                        .setCurrency("huf")
                         .setAutomaticPaymentMethods(
                                 PaymentIntentCreateParams.AutomaticPaymentMethods
                                         .builder()
